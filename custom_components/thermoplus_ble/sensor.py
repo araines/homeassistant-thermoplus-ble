c45c5ebd51c5f8e4bcfa0303e971fed8f7b398e4
Andy Raines <andrew.raines@gmail.com>
2020-08-18 21:03:38 +0100

    Try pybluez

--- custom_components/thermoplus_ble/sensor.py
+++ custom_components/thermoplus_ble/sensor.py
@@ -8,6 +8,8 @@ from homeassistant.const import (
 )
 from homeassistant.helpers.entity import Entity
 
+from bluetooth.ble import DiscoveryService
+
 DOMAIN = "thermoplus_ble" 
 
 _LOGGER = logging.getLogger(__name__)
@@ -18,7 +20,46 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 #  scanner = BLEScanner()
 #  hass.bus.listen("homeassistant_stop", scanner.shutdown_handler)
 #  scanner.start(config)
+  svc = DiscoveryService()
+  discovery = svc.discover(10)
+  for u, n in dev_ble.items():
+    print(u, n)
   sleep(1)
+
+class BLEScanner:
+  """Bluetooth Low Energy Scanner."""
+
+  dumpthread = None
+
+  def start(self, config):
+    """Start scanning for devices."""
+    _LOGGER.debug("Starting HCIdump thread")
+    self.dumpthread = HCIdump(
+      dumplist=self.hcidump_data,
+      interface=0,
+    )
+    self.dumpthread.start()
+
+  def stop(self):
+    """Stop scanning for devices."""
+    _LOGGER.debug("Stopping HCIdump thread")
+    if self.dumpthread is None:
+      return True
+    if self.dumpthread.isAlive():
+      self.dumpthread.join()
+      if self.dumpthread.isAlive():
+        _LOGGER.error("Waiting for HCIdump thread took too long")
+        return False
+      else:
+        self.dumpthread = None
+    return True
+
+  def shutdown_handler(self, event):
+    """Shutdown threads."""
+    _LOGGER.debug("Shutting down: %s", event)
+    self.stop()
+    
+
 
 class TemperatureSensor(Entity):
   """Representation of a Temperature Sensor."""
